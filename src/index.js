20de80089104dad92870b45f3f8410f12785daf6
Ana Berg <ana.berg99@gmail.com>
2020-09-26 02:20:21 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(
+  'mongodb+srv://ozwin:03061999@cluster0.zhdlj.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
